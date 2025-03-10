257367ba77adae00754b36df1d1630062e61330c
Mohammed <moham1515@gmail.com>
2025-03-11 00:48:24 +0400

    refactor(web): add content

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <div className="TitleContainer" style={{ position: "relative" }}>
+          <p className="EnglishText1">Soon</p>
+          <p className="ArabicText">قريبـــــــــاً</p>
+        </div>
+        <div className="TitleContainer " style={{ position: "relative" }}>
+          <p className="EnglishText2">We Reach You</p>
+          <p className="ArabicText">نصــــــــــــــــل إليكم</p>
+        </div>
+      </div>
     </div>
   );
 }
